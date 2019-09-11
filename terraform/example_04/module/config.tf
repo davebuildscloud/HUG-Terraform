46569ff6db43229941b85f9e14607a108742426a
Dave Stauffacher <davebuildscloud@gmail.com>
2019-09-11 07:03:53 -0700

    Update config.tf

--- terraform/example_04/module/config.tf
+++ terraform/example_04/module/config.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region  = "us-east-1"
-#  version = "1.60.0"
-   version = "2.7.0"
+  version = "1.60.0"
+#  version = "2.7.0"
 }
 
-#terraform {
-#  required_version = "0.11.8"
-#}
+terraform {
+  required_version = "0.11.8"
+}
